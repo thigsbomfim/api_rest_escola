aa6db6fdef9d7041ece955e4ae95256102e2b574
Thiago Bomfim <thigs.bomfim@gmail.com>
2023-05-18 22:10:39 -0300

    alterando caminho

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ class App {
     this.app.use(cors());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
-    this.app.use(express.static(resolve(__dirname, 'uploads')));
+    this.app.use(express.static(resolve(__dirname, 'uploads', 'images')));
   }
 
   routes() {
